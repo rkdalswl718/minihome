ec2830ff1159181cd849da14d2417b342aa6c6af
rkdalswl718 <rkdalswl070718@gmail.com>
2023-07-11 03:05:13 +0900

    ADD :: 로드시 방문수 증가

--- mini/index.js
+++ mini/index.js
@@ -1,3 +1,14 @@
+const todayDisplay = document.getElementById("today");
+const totalDisplay = document.getElementById("total");
+let today = 1;
+
+
+window.addEventListener('DOMContentLoaded',() => {
+  today ++;
+  todayDisplay.innerText = today;
+})
+
+
 window.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('audio');
     audio.play();
