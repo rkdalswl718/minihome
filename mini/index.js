029eab9f2edc0952f03ec459e06c1afaac05a890
rkdalswl718 <rkdalswl070718@gmail.com>
2023-07-11 03:11:10 +0900

    FIX :: 로컬스토리지 저장하여 증가

--- mini/index.js
+++ mini/index.js
@@ -1,15 +1,31 @@
-const todayDisplay = document.getElementById("today");
 const totalDisplay = document.getElementById("total");
-let today = 1;
+const todayDisplay = document.getElementById("today");
+let today = localStorage.getItem('today') || 1;
 
+const currentDate = new Date();
+const lastResetDate = localStorage.getItem('lastResetDate');
+const resetTime = 0;
 
-window.addEventListener('DOMContentLoaded',() => {
-  today ++;
+if (!lastResetDate || currentDate.getDate() > parseInt(lastResetDate)) {
+  today = 1;
+  localStorage.setItem('lastResetDate', currentDate.getDate());
+}
+
+const increaseToday = () => {
+  today++;
   todayDisplay.innerText = today;
-})
+  localStorage.setItem('today', today);
+};
+
+const increaseTotal = () => {
+  total++;
+}
 
 
 window.addEventListener('DOMContentLoaded', () => {
     const audio = document.getElementById('audio');
     audio.play();
   });
+
+
+  increaseToday();
